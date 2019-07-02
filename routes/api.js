07a6f306f8299a0d5ec81d3d90982ce6ae0d5b0d
Korawit <korawit.p@gmail.com>
2019-07-02 12:15:04 +0700

    Starter template

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* SIS APIs sandbox */
 router.get('/', (req, res, next) => {
-  const data = { username: 'korawit' };
+  const data = { username: 'korawit', role: 'lecturer' };
   res.status(200)
     .json({
       status: 'success',
