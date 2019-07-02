1338825ee750c2e2ebb733f130b0ec348a488c8c
Korawit <korawit.p@gmail.com>
2019-07-02 12:08:03 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var apiRouter = require('./routes/api');
+
+var app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/api', apiRouter);
+
+var port = process.env.PORT || 8080;
+
+app.listen(port, function () {
+    console.log('App is running on http://localhost:' + port);
+});
+
+module.exports = app;
---
+++ routes/api.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+
+/* SIS APIs sandbox */
+router.get('/', (req, res, next) => {
+  const data = { username: 'korawit' };
+  res.status(200)
+    .json({
+      status: 'success',
+      data: data
+    });
+});
+
+module.exports = router;
