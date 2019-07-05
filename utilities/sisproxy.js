4f06ac524e3176aa5a611708b979ba00806f320b
Korawit Prutsachainimmit <korawit.p@gmail.com>
2019-07-05 16:41:52 +0700

    Token renewal

--- utilities/sisproxy.js
+++ utilities/sisproxy.js
@@ -1,22 +1,42 @@
 const axios = require('axios');
-const baseURL = 'https://api2.phuket.psu.ac.th/sis/';
+const config = require('./config.json');
 const whitelist = require('./sandbox.json').whitelist;
+const baseURL = config.baseURL;
+
+
+async function renewToken() {
+    const targetURL = config.tokenURL;
+    const option = {
+        headers: {
+            'username': config.authentication.username,
+            'password': config.authentication.password
+        }
+    };
+    const response = await axios.get(targetURL, option);
+    return response.data;
+}
+
 
 async function getClassSchedule(studentID, eduTerm, eduYear) {
+  
+    const token = await renewToken();
 
-    console.log(whitelist);
-    const allow = whitelist.includes(studentID);
+    if (!token.AccessToken) {
+        throw new Error("Token renewal failed");
+    }
     
-    if(allow == false){
+    const allow = whitelist.includes(studentID);
+
+    if (allow == false) {
         throw new Error("Permission denined");
     }
 
     const targetURL = baseURL + `students/${studentID}/class-schedules/${eduYear}/${eduTerm}`;
-    var config = {
-        headers: { 'AccessToken': '5bba5c62d4f74f8485ea8b5f2190f9ee' }
-    };  
-    const response = await axios.get(targetURL, config);
-    
+    var option = {
+        headers: { 'AccessToken': token.AccessToken }
+    };
+    const response = await axios.get(targetURL, option);
+
     return response.data;
 }
 
