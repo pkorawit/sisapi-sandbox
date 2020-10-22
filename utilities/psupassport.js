1e071e901d8b9c2eaf4665e253a5d7e1a4af1875
Korawit Prutsachainimmit <korawit.p@phuket.psu.ac.th>
2020-10-22 15:11:17 +0700

    Update sandbox

--- utilities/psupassport.js
+++ utilities/psupassport.js
@@ -4,25 +4,23 @@ const whitelist = require('./sandbox.json').whitelist;
 
 async function authenticate(username, password) {
     
-    // const allow = whitelist.includes(username);
-    // if (allow == false) {
-    //     throw new Error("Permission denined");
-    // }
+    const allow = whitelist.includes(username);
+    if (allow == false) {
+        throw new Error("Permission denined");
+    }
 
-    // if(allow){
-    //     var url = config.psuPassport.authentication.wsdl;
-    //     var args = {
-    //         username: username,
-    //         password: password
-    //     };
+    if(allow){
+        var url = config.psuPassport.authentication.wsdl;
+        var args = {
+            username: username,
+            password: password
+        };
         
-    //     const client = await soap.createClientAsync(url);
-    //     const output = await client.AuthenticateAsync(args);
-    //     console.log(output);        
-    //     return output[0].AuthenticateResult;
-    // }
-
-    return true;
+        const client = await soap.createClientAsync(url);
+        const output = await client.AuthenticateAsync(args);
+        console.log(output);        
+        return output[0].AuthenticateResult;
+    }
 
 }
 
